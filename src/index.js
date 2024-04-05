a8c639639b9f9128a46b15064c08490c29c6f6e4
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-04-05 20:52:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { persistor, store } from './reduxConfig/store';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/money-guard">
+        <App />
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={true}
+          newestOnTop={true}
+          closeOnClick
+          theme="colored"
+        />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
